5b203ad426a9a696fb8a37db87c11ee022072a4b
joaomodric <joao.403114@alunosatc.edu.br>
2021-08-30 14:21:53 -0300

    Create exer28.py

---
+++ exer28.py
@@ -0,0 +1,8 @@
+#Quadrado de tres valores lidos
+n1 = float(input("Digite o primeiro numero: "))
+n2 = float(input("Digite o segundo numero: "))
+n3 = float(input("Digite o terceiro numero: "))
+q1 = (n1**2)
+q2 = (n2**2)
+q3 = (n3**2)
+input("A soma do quadrado de todos os valores lidos resulta em: {}".format(q1+q2+q3))

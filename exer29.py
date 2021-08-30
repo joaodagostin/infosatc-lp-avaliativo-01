6231325ce573e2654bb48af976327d9beede1530
joaomodric <joao.403114@alunosatc.edu.br>
2021-08-30 14:25:12 -0300

    Create exer29.py

---
+++ exer29.py
@@ -0,0 +1,7 @@
+#Calculo de media
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+n3 = float(input("Digite a terceira nota: "))
+n4 = float(input("Digite a quarta nota: "))
+media = (n1+n2+n3+n4)/4
+input("A média é: {}".format(media))
